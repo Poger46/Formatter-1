9e0007645a925d48922c1906de5705b98cd41080
Poger46 <53600012+Poger46@users.noreply.github.com>
2022-06-09 23:32:37 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,19 @@
 public class Main {
     public static void main(String[] args){
         FormattedInput input = new FormattedInput();
-         input.sscanf("%d %s %c %f", "1 qwerty d 1.999999999999999999999");
+        // input.sscanf("%d %s %c %f", "1 qwerty d 1.999999999999999999999");
         FormattedInput input2 = new FormattedInput();
-         input2.scanf("%d %s %c %f");
+        // input2.scanf("%d %s %c %f");
 
         if(input.equals(input2))
             System.out.println(1);
         else
             System.out.println(0);
 
+         EncodingConverter ec = new EncodingConverter();
+        ec.EConverter("C:\\Users\\Poger46\\untitled4\\src\\input.txt","C:\\Users\\Poger46\\untitled4\\src\\output.txt", "utf8","utf16" );
+
+        
     }
 
 }
